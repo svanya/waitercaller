2741c4a18ca804ee869dd9add3deaed509bd288f
stefan <stefan@stefan.com>
2017-12-11 03:21:59 +0100

    Testik

--- waitercaller.py
+++ waitercaller.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_login import LoginManager
+from flask_login import login_required
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
 
 
 @app.route("/")
 def home():
-    return "Under construction"
+    return render_template("home.html")
+
+
+@app.route("/account")
+@login_required
+def account():
+    return "You are logged in"
 
 
 if __name__ == '__main__':
